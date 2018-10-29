1083fac26c4990789a3857f85c060f1a272cf246
Angela J. Holden <angelajholden@gmail.com>
2018-10-29 17:33:36 -0500

    getting ready to start this build

--- index.php
+++ index.php
@@ -8,17 +8,61 @@
     <title>Angela 2018</title>
     <meta name="description" content="This is the Angela 2018 Theme.">
     <link href='http://fonts.googleapis.com/css?family=Tinos:400,700,400italic' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.4.2/css/all.css" integrity="sha384-zhaLg9HKxTxDljOPXpWHGn91XMDH+sYAWRSgvzHes290/ISyrNicGrd6BInTnx3L" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
     <link rel="shortcut icon" href="favicon.ico">
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script type="text/javascript" src="compiled/base.min.js"></script>
     <!--[if lt IE 9]>
     <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
   </head>
 
-  <body>
-  	<header><h1>Header</h1></header>
+  <body class="home">
+
+  	<section class="hero">
+
+  		<div class="wrap">
+
+		  	<header class="header">
+		  		<h1 class="title">Angela J. Holden</h1>
+		  		<div class="menu-open">
+		  			<span class="fas fa-th" aria-label="menu-open"></span>
+		  		</div>
+		  		<nav class="primary-menu">
+		  			<div class="menu-close">
+		  				<span class="fal fa-times" aria-label="menu-close"></span>
+		  			</div>
+		  			<ul>
+		  				<li><a href="#">Home</a></li>
+		  				<li><a href="#">About</a></li>
+		  				<li><a href="#">Resume</a></li>
+		  				<li><a href="#">Contact</a></li>
+		  			</ul>
+		  		</nav>
+		  	</header>
+
+	  		<h2 class="intro"></h2>
+
+	  		<div class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
+
+	  		<nav class="social-menu">
+	  			<ul>
+	  				<li><a href="#"><span class="fab fa-linkedin-in"></span>LinkedIn</a></li>
+	  				<li><a href="#"><span class="fab fa-pinterest-p"></span>Pinterest</a></li>
+	  				<li><a href="#"><span class="fab fa-instagram"></span>Instagram</a></li>
+	  				<li><a href="#"><span class="fab fa-github-alt"></span>Github</a></li>
+	  				<li><a href="#"><span class="fab fa-etsy"></span>Etsy</a></li>
+	  			</ul>
+	  		</nav>
+
+	  		<figure>
+	  			<img src="https://picsum.photos/600/600" alt="this is some alt text" width="600" height="600">
+	  		</figure>
+
+  		</div>
+  		
+  	</section>
+
   	<main>Main</main>
   	<footer>Footer</footer>
   </body>
